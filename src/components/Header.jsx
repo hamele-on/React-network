2ccada00b05169e47fe9e62aac094ad03fae1226
hamele-on <hamele_on@list.ru>
2021-11-04 17:27:26 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => {
+    return(
+    <header className='header'>
+        <img src='https://bumper-stickers.ru/33729-thickbox_default/tatuirovka-golova-orla.jpg' />
+    </header>
+    );
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Profile  = () => {
+    return (
+        <div className='content'>
+        <div>
+          <img src='https://image.jimcdn.com/app/cms/image/transf/dimension=1280x10000:format=png/path/sa76e733a9e22af6d/image/i73bc67433a478723/version/1518633792/image.png' />
+        </div>
+        <div>AVA + DESCRIPTION</div>
+        <div>
+          MY POSTS
+          <div>
+            NEW POST
+          </div>
+          <div>
+            <div>
+              Post 1
+            </div>
+
+            <div>
+              Post 2
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    );
+
+}
+
+export default Profile;
