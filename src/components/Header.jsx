24c6e4932ed745f6720cc5d4b014155b6d84edc0
hamele-on <hamele_on@list.ru>
2021-11-04 20:52:35 +0300

    worck whis img

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
     return(
     <header className='header'>
-        <img src='https://bumper-stickers.ru/33729-thickbox_default/tatuirovka-golova-orla.jpg' />
+        <img src ='https://bumper-stickers.ru/33729-thickbox_default/tatuirovka-golova-orla.jpg' alt="" />
     </header>
     );
 }
